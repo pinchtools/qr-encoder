bc909b1f5b52a82670834141a745894f635265d4
vincent <pelle.vincent@gmail.com>
2015-11-05 15:47:33 +0100

    first release

---
+++ test/index.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var chai = require('chai');
+chai.should();
+
+var qrEncoder;
+
+beforeEach(function () {
+  qrEncoder = require('../index');
+});
+
+describe('qr-encoder', function () {
+
+	describe('#encode()', function() {
+
+		it('is a function', function () {
+		    qrEncoder.should.have.property('encode');
+		    qrEncoder.encode.should.be.a('function');
+		});
+
+		it('throws an error if no string passed', function () {
+		    (function () {
+		        qrEncoder.encode();
+		    }).should.Throw('no string passed');
+		});
+
+		it('accepts only valid ecclevel', function() {
+		    (function () {
+		        qrEncoder.encode("Hi", 5);
+		    }).should.Throw('wrong ecclevel');
+		});
+
+
+		it('returns a valid array', function () {
+		    var result = qrEncoder.encode("Hi",1);
+			
+		    result.should.be.a('array');
+
+		    //array length shoud be between min & max nb of modules
+		    //supported by a qrcode
+		    result.should.have.length.within(21,177);
+
+		    //lines length should be equals to array length
+		    result.forEach(line => {
+		      line.should.have.length(result.length);
+		    });
+
+		});
+	});
+
+});
